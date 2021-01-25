74601403239fcc83e87f9d412b2ded0547e50bea
anaisvh <a.vhuelamo@gmail.com>
2021-01-25 11:17:50 +0100

    actualization

--- src/services/api/server.py
+++ src/services/api/server.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, request, render_template
-import os,sys
+import os, sys
 import pandas as pd
 
 z_file = __file__
@@ -9,7 +9,7 @@ for i in range(4):
     z_file = os.path.dirname(z_file)
 sys.path.append(z_file)
 
-from src.utils.folders_tb import jsonlink_df
+from src.utils.folders_tb import jsonlink_df, read_json
 from src.utils.mining_data_tb import filter_df, df_covid
 
 covid = jsonlink_df('https://covid.ourworldindata.org/data/owid-covid-data.json').T
@@ -21,29 +21,53 @@ covid_grouped = covid_grouped.astype(int).rename(columns={"data.new_cases": "n_c
 covid_grouped.to_json('n_c_averages.json')
 
 app = Flask(__name__) 
-app.config["DEBUG"] = True
 
-@app.route('/c_json')
+@app.route("/")
 def home():
-    n_c_averages = covid_grouped.to_json()
-    return n_c_averages
+    return app.send_static_file('greet.html')
 
-@app.route('/give_me_id', methods=['GET'])
-def group_id():
-    n = request.args['id']
-    if n == "A86":
+@app.route("/greet")
+def greet():
+    a = request.args.get('name')
+    if a == "A86":
         appDict = {'token': 'A43649037'}
         app_json = json.dumps(appDict)
         return app_json
     else:
         return "No es el identificador correcto"
 
+def json_A():
+    n_c_averages = covid_grouped.to_json()
+    return n_c_averages
+
 @app.route('/give_me_token', methods=['GET'])
 def group_token():
     s = request.args["token"]
     if s == "A43649037":
-        return home()
+        return json_A()
     else:
         return "No es el identificador correcto"
 
-app.run() #ip 0000 crear archivo settings
+def main():
+    print("---------STARTING PROCESS---------")
+    print(__file__)
+
+    settings_file = os.path.dirname(__file__) + os.sep + "settings.json"
+    print(settings_file)
+    # Load json from file
+    json_readed = read_json(fullpath=settings_file)
+    
+    # Load variables from jsons
+    SERVER_RUNNING = json_readed["server_running"]
+    print("SERVER_RUNNING", SERVER_RUNNING)
+    if SERVER_RUNNING:
+        DEBUG = json_readed["debug"]
+        HOST = json_readed["host"]
+        PORT_NUM = json_readed["port"]
+        app.run(debug=DEBUG, host=HOST, port=PORT_NUM)
+    else:
+        print("Server settings.json doesn't allow to start server. " + 
+              "Please, allow it to run it.")
+
+if __name__ == "__main__":
+    main()
